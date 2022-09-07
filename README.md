17dae79066da6ff3878a493ccc14473b42dd823a
9ice_guy <clintonufere@gmail.com>
2022-09-07 04:41:32 +0100

    File upload

---
+++ README.md
@@ -0,0 +1,7 @@
+## Chef Landing Page For A Friend
+<p>
+This is a simple chef landing page built for a friend to use for his presentation.
+
+<p align="center">
+<img src="https://github.com/clinton9ice/Chef-Landing-page/blob/385e7d43d978af7b45140785c9705d43d3924a86/src/assets/photo-1583394293214-28ded15ee548-removebg-preview.png?raw=true"/>
+</p>
---
+++ src/assets/script.js
@@ -0,0 +1,32 @@
+
+
+const items = [
+  'Incredible...',
+  'Amazing...',
+  'Wonderful...',
+  'Fantastic Chef!!'
+]
+const app = document.getElementById('effect')
+let count = 0
+let index = 0
+
+let typingEffect = () => {
+    let text = items[index];
+
+  if (count < text.length) {
+    setTimeout(() => {
+      app.innerHTML += text[count]
+      count++
+      typingEffect()
+    }, Math.floor(Math.random(10) * 100))
+  } else {
+    count = 0;
+    index = (index + 1 < items.length) ? index + 1 : 0
+    setTimeout(() => {
+      app.innerHTML = ''
+      typingEffect()
+    }, 1500)
+  }
+}
+
+typingEffect()
